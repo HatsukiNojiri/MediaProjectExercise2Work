65d68a7bc3527806a04815190641e11e70bab1e1
HatsukiNojiri <hatsuki.njr@icloud.com>
2022-06-29 15:47:53 +0900

    contine check10

---
+++ Assets/InputSpeech.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputSpeech : MonoBehaviour
+{
+    public GameObject Cube;
+    public Text outputText;
+    private Text inputSpeech;
+    private string str1 = "";
+    private int strNum = 0;
+
+    //Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        inputSpeech = this.GetComponent<Text>();
+
+        //追加の音声入力がされた場合
+        if(strNum != inputSpeech.text.Length)
+        {
+            str1 = inputSpeech.text.Substring(strNum); //追加音声の切り出し
+            outputText.text = str1; //音声の出力
+            strNum = inputSpeech.text.Length; //音声の文字数
+
+            Vector3 position = Cube.transform.position; //Cubeの座標取得
+            //文字列の比較,Cubeの移動
+            if(str1 == "上")
+            {
+                position.z += 1.0f;
+                Cube.transform.position = position;
+            }
+            if (str1 == "下")
+            {
+                position.z -= 1.0f;
+                Cube.transform.position = position;
+            }
+            if (str1 == "左")
+            {
+                position.x -= 1.0f;
+                Cube.transform.position = position;
+            }
+            if (str1 == "右")
+            {
+                position.x += 1.0f;
+                Cube.transform.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/audiorecognition.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+//参考： https://docs.microsoft.com/ja-jp/windows/mixed-reality/voice-input-in-unity
+
+public class audiorecognition : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 認識したい単語の登録
+        keywords.Add("うえ", () => { }); //{}の中に処理内容を記述してもよい
+        keywords.Add("した", () => { }); //その場合は、keywordAction.Invoke()で呼び出す
+        keywords.Add("ひだり", () => { });
+        keywords.Add("みぎ", () => { });
+
+        //音声認識イベントの登録
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhaseRecognized;
+
+        keywordRecognizer.Start();
+        Debug.Log("音声認識開始");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //単語が読み上げられた場合のイベント処理
+    private void KeywordRecognizer_OnPhaseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        /* keywords.Addの{}内に処理を記入した場合
+        if(keywords.TryGetValue(args.text, out keywordAction))
+        {
+        keywordAction.Invoke();
+        }
+        */
+        switch (args.text)
+        {
+            case "うえ":
+                Debug.Log("うえ");
+                break;
+            case "した":
+                Debug.Log("した");
+                break;
+            case "ひだり":
+                Debug.Log("ひだり");
+                break;
+            case "みぎ":
+                Debug.Log("みぎ");
+                break;
+        }
+    }
+}
